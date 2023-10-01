554dc486b52de695847ee0ef8473bebc9b60ba06
yuki funasaka <yuki.funasaka.mithra@gmail.com>
2023-10-02 00:48:51 +0900

    yarnライブラリを学習

---
+++ sum/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * 数値の配列を受け取って、その要素の合計を返す関数
+ */
+function add(numbers) {
+  let result = 0;
+  for (const num of numbers) {
+    result = result + num;
+  }
+  return result;
+}
+
+module.exports = {
+  add: add
+};
